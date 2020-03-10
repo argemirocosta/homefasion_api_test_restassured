39a16d0f7a0d5c4e7c6e32f566f9132e971f896e
Argemiro <argemiro.pcosta@gmail.com>
2020-03-09 21:47:15 -0300

    - Adicionando testes de inserção e cancelamento de pagamentos.

--- src/br/com/homefasion_api_test_restassure/shared/PagamentoEndPoints.java
+++ src/br/com/homefasion_api_test_restassure/shared/PagamentoEndPoints.java
@@ -6,14 +6,16 @@ public class PagamentoEndPoints {
 
     private PagamentoEndPoints() {}
 
-    public static final String GET_PAGAMENTO = ENDPOINT_PRINCIPAL_PARA_TESTE+"pagamento/";
+    public static final String PAGAMENTO = ENDPOINT_PRINCIPAL_PARA_TESTE+"pagamento/";
 
-    public static final String GET_PAGAMENTO_NO_AR = GET_PAGAMENTO +"servidor";
+    public static final String GET_PAGAMENTO_NO_AR = PAGAMENTO +"servidor";
 
-    public static final String GET_PAGAMENTO_ESPECIFICO = GET_PAGAMENTO +"374";
+    public static final String PAGAMENTO_ESPECIFICO = PAGAMENTO +"374";
 
-    public static final String GET_PAGAMENTOS_POR_CLIENTE = GET_PAGAMENTO +"cliente/79";
+    public static final String GET_PAGAMENTOS_POR_CLIENTE = PAGAMENTO +"cliente/79";
 
-    public static final String GET_PAGAMENTOS_POR_VENDA = GET_PAGAMENTO +"venda/167";
+    public static final String GET_PAGAMENTOS_POR_VENDA = PAGAMENTO +"venda/167";
+
+    public static final String PUT_PAGAMENTO_CANCELAR = PAGAMENTO +"cancela/";
 
 }
--- test/br.com.homefasion_api_test_restassure/resources/PagamentoTest.java
+++ test/br.com.homefasion_api_test_restassure/resources/PagamentoTest.java
@@ -21,13 +21,13 @@ public class PagamentoTest {
     @Category({PositiveTest.class})
     public void deveVerificarSePagamentoNoAr(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_PAGAMENTO_NO_AR)
+                    .request(Method.GET, GET_PAGAMENTO_NO_AR)
                 .then()
-                .statusCode(200)
-                .body(is("Servidor no ar"));
+                    .statusCode(200)
+                    .body(is("Servidor no ar"));
     }
 
     @Test
@@ -42,61 +42,105 @@ public class PagamentoTest {
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarTodosOsPagamento(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_PAGAMENTO)
+                    .request(Method.GET, PAGAMENTO)
                 .then()
-                .statusCode(200)
-                .body("id[0]", is(305))
-                .body("valorPago[0]", is(37f));
+                    .statusCode(200)
+                    .body("id[0]", is(305))
+                    .body("valorPago[0]", is(37f));
     }
 
     @Test
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarPagamentoEspecifico(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_PAGAMENTO_ESPECIFICO)
+                    .request(Method.GET, PAGAMENTO_ESPECIFICO)
                 .then()
-                .statusCode(200)
-                .body("id", is(374))
-                .body("idVenda", is(179))
-                .body("valorPago", is(37f))
-                .body("dataPagamento", is("18/04/2018"))
-                .body("usuario.id", is(1));
+                    .statusCode(200)
+                    .body("id", is(374))
+                    .body("idVenda", is(179))
+                    .body("valorPago", is(37f))
+                    .body("dataPagamento", is("18/04/2018"))
+                    .body("usuario.id", is(1));
     }
 
     @Test
     @Category({PositiveTest.class})
     public void deveListarPagamentoPorCliente(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_PAGAMENTOS_POR_CLIENTE)
+                    .request(Method.GET, GET_PAGAMENTOS_POR_CLIENTE)
                 .then()
-                .statusCode(200)
-                .body("id[0]", is(336))
-                .body("idVenda[0]", is(167))
-                .body("valorPago[0]", is(80f));
+                    .statusCode(200)
+                    .body("id[0]", is(336))
+                    .body("idVenda[0]", is(167))
+                    .body("valorPago[0]", is(80f));
     }
 
     @Test
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarPagamentoPorVenda(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_PAGAMENTOS_POR_VENDA)
+                    .request(Method.GET, GET_PAGAMENTOS_POR_VENDA)
                 .then()
-                .statusCode(200)
-                .body("id[0]", is(336))
-                .body("idVenda[0]", is(167))
-                .body("dataPagamento[0]", is("27/02/2018"))
-                .body("valorPago[0]", is(80f));
+                    .statusCode(200)
+                    .body("id[0]", is(336))
+                    .body("idVenda[0]", is(167))
+                    .body("dataPagamento[0]", is("27/02/2018"))
+                    .body("valorPago[0]", is(80f));
     }
+
+    @Test
+    @Category({PositiveTest.class})
+    public void deveRealizarPagamento(){
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .body("{\"idVenda\": 374, \"valorPago\": 1,\"dataPagamento\": \"09/03/2020\",\"idUsuario\": \"155\", \"cancelada\": \"false\", \"dataHoraCancelamento\": null }")
+                .when()
+                    .request(Method.POST, PAGAMENTO)
+                .then()
+                    .statusCode(201);
+    }
+
+    @Test
+    @Category({NegativeTest.class})
+    public void naoDeveRealizarPagamentoSemVenda(){
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .body("{\"valorPago\": 1,\"dataPagamento\": \"09/03/2020\",\"idUsuario\": \"155\", \"cancelada\": \"false\", \"dataHoraCancelamento\": null }")
+                .when()
+                    .request(Method.POST, PAGAMENTO)
+                .then()
+                    .statusCode(400);
+    }
+
+    @Test
+    @Category({PositiveTest.class})
+    public void deveRealizarCancelamentoDePagamento(){
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .pathParam("codigoPagamento", 793)
+                .when()
+                    .put(PUT_PAGAMENTO_CANCELAR+"{codigoPagamento}")
+                .then()
+                    .statusCode(204);
+    }
+
+
 }
