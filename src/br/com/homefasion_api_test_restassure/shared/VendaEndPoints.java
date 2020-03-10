aa807fca957ff7ebb2d53f91920da0a7ed444696
Argemiro <argemiro.pcosta@gmail.com>
2020-03-09 22:08:46 -0300

    - Adicionando testes de inserção e cancelamento de vendas.

--- src/br/com/homefasion_api_test_restassure/shared/VendaEndPoints.java
+++ src/br/com/homefasion_api_test_restassure/shared/VendaEndPoints.java
@@ -6,12 +6,14 @@ public class VendaEndPoints {
 
     private VendaEndPoints() {}
 
-    public static final String GET_VENDA = ENDPOINT_PRINCIPAL_PARA_TESTE+"venda/";
+    public static final String VENDA = ENDPOINT_PRINCIPAL_PARA_TESTE+"venda/";
 
-    public static final String GET_VENDA_NO_AR = GET_VENDA +"servidor";
+    public static final String GET_VENDA_NO_AR = VENDA +"servidor";
 
-    public static final String GET_VENDA_ESPECIFICA = GET_VENDA +"374";
+    public static final String VENDA_ESPECIFICA = VENDA +"374";
 
-    public static final String GET_VENDAS_POR_USUARIO = GET_VENDA +"usuario/155";
+    public static final String GET_VENDAS_POR_USUARIO = VENDA +"usuario/155";
+
+    public static final String PUT_VENDA_CANCELAR = VENDA +"cancela/";
 
 }
--- test/br.com.homefasion_api_test_restassure/resources/VendaTest.java
+++ test/br.com.homefasion_api_test_restassure/resources/VendaTest.java
@@ -21,13 +21,13 @@ public class VendaTest {
     @Category({PositiveTest.class})
     public void deveVerificarSeVendaNoAr(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_VENDA_NO_AR)
+                    .request(Method.GET, GET_VENDA_NO_AR)
                 .then()
-                .statusCode(200)
-                .body(is("Servidor no ar"));
+                    .statusCode(200)
+                    .body(is("Servidor no ar"));
     }
 
     @Test
@@ -42,47 +42,76 @@ public class VendaTest {
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarTodosAsVendas(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_VENDA)
+                    .request(Method.GET, VENDA)
                 .then()
-                .statusCode(200)
-                .body("id[0]", is(202))
-                .body("valor[0]", is(207f));
+                    .statusCode(200)
+                    .body("id[0]", is(202))
+                    .body("valor[0]", is(207f));
     }
 
     @Test
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarVendaEspecifica(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_VENDA_ESPECIFICA)
+                    .request(Method.GET, VENDA_ESPECIFICA)
                 .then()
-                .statusCode(200)
-                .body("id", is(374))
-                .body("cliente.id", is(234))
-                .body("qtd", is(2))
-                .body("valor", is(105.5f))
-                .body("data", is("01/01/2020"))
-                .body("idUsuario", is(155))
-                .body("cancelada", is(true));
+                    .statusCode(200)
+                    .body("id", is(374))
+                    .body("cliente.id", is(234))
+                    .body("qtd", is(2))
+                    .body("valor", is(105.5f))
+                    .body("data", is("01/01/2020"))
+                    .body("idUsuario", is(155))
+                    .body("cancelada", is(true));
     }
 
     @Test
     @Category({PositiveTest.class})
     public void deveListarVendaPorUsuario(){
         given()
-                .log().all()
-                .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .log().all()
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
                 .when()
-                .request(Method.GET, GET_VENDAS_POR_USUARIO)
+                    .request(Method.GET, GET_VENDAS_POR_USUARIO)
                 .then()
-                .statusCode(200)
-                .body("id[0]", is(370))
-                .body("qtd[0]", is(2))
-                .body("valor[0]", is(105.5f));
+                    .statusCode(200)
+                    .body("id[0]", is(370))
+                    .body("qtd[0]", is(2))
+                    .body("valor[0]", is(105.5f));
     }
+
+    @Test
+    @Category({PositiveTest.class})
+    public void deveRealizarVenda(){
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .body("{ \"idCliente\": 234, \"valor\": 300, \"qtd\": 2, \"data\": \"01/01/2020\", \"idUsuario\": 155, \"cancelada\": \"false\", \"dataHoraCancelamento\": null }")
+                .when()
+                    .request(Method.POST, VENDA)
+                .then()
+                    .statusCode(201);
+    }
+
+    @Test
+    @Category({PositiveTest.class})
+    public void deveRealizarCancelamentoDeVenda(){
+        given()
+                    .log().all()
+                    .contentType("application/json")
+                    .auth().basic(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN)
+                    .pathParam("codigoVenda", 382)
+                .when()
+                    .put(PUT_VENDA_CANCELAR+"{codigoVenda}")
+                .then()
+                    .statusCode(204);
+    }
+
 }
