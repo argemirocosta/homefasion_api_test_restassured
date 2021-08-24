1f701993c2eee79176f56e4bbe262020991a9776
Argemiro <argemiro.pcosta@gmail.com>
2021-08-24 17:05:47 -0300

    Refatorando PagamentoTest

--- src/main/br/com/homefasion_api_test_restassured/tests/PagamentoTest.java
+++ src/main/br/com/homefasion_api_test_restassured/tests/PagamentoTest.java
@@ -13,7 +13,6 @@ import org.junit.experimental.categories.Category;
 import static br.com.homefasion_api_test_restassured.shared.PagamentoEndPoints.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertEquals;
 
 public class PagamentoTest extends BaseTest {
@@ -22,13 +21,11 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class})
     public void deveVerificarSePagamentoNoAr(){
         given()
-                    .log().all()
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                 .when()
-                    .request(Method.GET, GET_PAGAMENTO_NO_AR)
+                    .get(GET_PAGAMENTO_NO_AR)
                 .then()
                     .statusCode(200)
-                    .time(lessThan(3000L))
                     .body(is("Servidor no ar"));
     }
 
@@ -44,10 +41,9 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarTodosOsPagamento(){
         given()
-                    .log().all()
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                 .when()
-                    .request(Method.GET, PAGAMENTO)
+                    .get(PAGAMENTO)
                 .then()
                     .statusCode(200)
                     .body("id[0]", is(305))
@@ -58,10 +54,9 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarPagamentoEspecifico(){
         given()
-                    .log().all()
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                 .when()
-                    .request(Method.GET, PAGAMENTO_ESPECIFICO)
+                    .get(PAGAMENTO_ESPECIFICO)
                 .then()
                     .statusCode(200)
                     .body("id", is(374))
@@ -75,10 +70,9 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class})
     public void deveListarPagamentoPorCliente(){
         given()
-                    .log().all()
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                 .when()
-                    .request(Method.GET, GET_PAGAMENTOS_POR_CLIENTE)
+                    .get(GET_PAGAMENTOS_POR_CLIENTE)
                 .then()
                     .statusCode(200)
                     .body("id[0]", is(336))
@@ -90,10 +84,9 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class, SmokeTest.class})
     public void deveListarPagamentoPorVenda(){
         given()
-                    .log().all()
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                 .when()
-                    .request(Method.GET, GET_PAGAMENTOS_POR_VENDA)
+                    .get(GET_PAGAMENTOS_POR_VENDA)
                 .then()
                     .statusCode(200)
                     .body("id[0]", is(336))
@@ -106,12 +99,11 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class})
     public void deveRealizarPagamento(){
         given()
-                    .log().all()
                     .contentType("application/json")
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                     .body("{\"idVenda\": 374, \"valorPago\": 1,\"dataPagamento\": \"09/03/2020\",\"idUsuario\": \"155\", \"cancelada\": \"false\", \"dataHoraCancelamento\": null }")
                 .when()
-                    .request(Method.POST, PAGAMENTO)
+                    .post(PAGAMENTO)
                 .then()
                     .statusCode(201);
     }
@@ -120,12 +112,11 @@ public class PagamentoTest extends BaseTest {
     @Category({NegativeTest.class})
     public void naoDeveRealizarPagamentoSemVenda(){
         given()
-                    .log().all()
                     .contentType("application/json")
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                     .body("{\"valorPago\": 1,\"dataPagamento\": \"09/03/2020\",\"idUsuario\": \"155\", \"cancelada\": \"false\", \"dataHoraCancelamento\": null }")
                 .when()
-                    .request(Method.POST, PAGAMENTO)
+                    .post(PAGAMENTO)
                 .then()
                     .statusCode(400);
     }
@@ -134,7 +125,6 @@ public class PagamentoTest extends BaseTest {
     @Category({PositiveTest.class})
     public void deveRealizarCancelamentoDePagamento(){
         given()
-                    .log().all()
                     .contentType("application/json")
                     .auth().basic(USER_FOR_TEST, PASSWORD_FOR_TEST)
                     .pathParam("codigoPagamento", 793)
